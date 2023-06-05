f69d2e565442953b650facd089d29952f501840f
Annylys Medeiros <annylysmedeiros@hotmail.com>
2023-06-04 21:06:18 -0300

    deploy Railway

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { getTasks, saveTask, getTask, updateTask, deleteTask, finishedTask } fro
 const routes = Router()
  
 routes.get('/home', (request: Request, response: Response) => {
-    return response.json({ message: 'Hello Turma 007!' })
+    return response.json({ message: 'On' })
 })
 routes.get('/tasks', getTasks)
 routes.post('/tasks', saveTask)
